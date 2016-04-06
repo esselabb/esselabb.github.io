d33dcefc959f08dd322de341364f7a8d14fd6e41
Trong Nghia <dnghia13@gmail.com>
2016-04-06 11:13:32 +0700

    add some library

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 var app = angular.module('myApp', []);
 app.controller('chatController', function($scope) {
     $scope.message = 'ok';
+    
+    $scope.enterMessage = function (keyEvent) {
+        if (keyEvent.which === 13)
+            alert('I am an alert');
+    };
 });
