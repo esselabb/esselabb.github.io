ef83d87be9fb8f3543664b8fae3aaa8884dfee99
Trong Nghia <dnghia13@gmail.com>
2016-04-06 11:15:45 +0700

    add some library

--- js/app.js
+++ js/app.js
@@ -2,8 +2,21 @@ var app = angular.module('myApp', []);
 app.controller('chatController', function($scope) {
     $scope.message = 'ok';
     
-    $scope.enterMessage = function (keyEvent) {
-        if (keyEvent.which === 13)
-            alert('I am an alert');
+    $scope.enterMessage = function() {
+        alert('I am an alert');
+    };
+});
+
+app.directive('myEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.myEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
     };
 });
