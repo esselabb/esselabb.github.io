12508b6d9a9c7c8e19752aab1589d86a0351fc2a
Trong Nghia <dnghia13@gmail.com>
2016-04-06 13:53:05 +0700

    test

--- js/app.js
+++ js/app.js
@@ -2,19 +2,50 @@ $( document ).ready(function() {
     
     var firebase = new Firebase('https://scorching-inferno-4736.firebaseio.com/');
     
+    var isBotttomMessage = true;
+    
+    $('#myModal').modal('show');
+    
+    $('#myModal').on('hide.bs.modal', function(e) {
+        if (!isValidUsername()) {
+            e.preventDefault();
+            
+        }
+    })
+    
+    $('#save_username_btn').click(function() {
+        $('#myModal').modal('hide');
+    });
+    
     firebase.on('child_added', function(snapshot) {
-        console.log(snapshot);
+        var data = snapshot.val();
+        if (typeof data != 'undefined' && typeof data.name != 'undefined' && typeof data.message != 'undefined') {
+            $('.message-response').find('.inner').append('<p><b>' + data.name + ':</b> ' + data.message + ' </p>');
+        }
+        if (isBotttomMessage) {
+            console.log($('.message-response').find('.inner').height());
+            $('.message-response').scrollTop($('.message-response').find('.inner').height());
+        }
     });
     
     $('#message_input').keypress(function(e) {
         if (e.which == 13) {
             var message = $('#message_input').val().trim();
             if (message != '') {
-                firebase.push({name: '', message: message});
+                firebase.push({name: $('#username_input').val().trim(), message: message});
             }
             $('#message_input').val('');
         }
     });   
+    
+    $('.message-response').scroll(function() {
+        isBotttomMessage = $('.message-response').scrollTop() >= $('.message-response').find('.inner').height() - 600 - 15 ;
+        console.log(isBotttomMessage);
+    });
+    
+    function isValidUsername() {
+        return $('#username_input').val().trim() != '';
+    };
     
 });
 
